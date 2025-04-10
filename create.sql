8f878d44f9c2e0f2add5ecc4004d63f137e295d8
marilynn12 <148828455+marilynn12@users.noreply.github.com>
2025-04-10 12:43:23 -0500

    Update create.sql
    finished create.sql

--- create.sql
+++ create.sql
@@ -18,8 +18,9 @@ CREATE TABLE Apartment_Complex (
   City VARCHAR(100) DEFAULT NULL,
   L_State VARCHAR(50) DEFAULT NULL, -- i changed this to L_State because state is a keyword
   PRIMARY KEY (Complex_Number),
-  FOREIGN KEY (Manager) REFERENCES Employee(ESSN) 
-  ON DELETE SET DEFAULT 
+  FOREIGN KEY (Manager)
+    REFERENCES Employee(ESSN) 
+    ON DELETE SET DEFAULT 
 );
 
 -- Apartment Table
@@ -28,8 +29,9 @@ CREATE TABLE Apartment (
   Room_Number INT NOT NULL DEFAULT 0000,
   Floor_Plan VARCHAR(50) DEFAULT NULL,
   PRIMARY KEY (Room_NUmber, Complex_Number),
-  FOREIGN KEY (Complex_Number) REFERENCES Apartment_Complex(Complex_Number) 
-  ON DELETE SET DEFAULT 
+  FOREIGN KEY (Complex_Number) 
+    REFERENCES Apartment_Complex(Complex_Number) 
+    ON DELETE SET DEFAULT 
 );
 
 -- Renters Table 
@@ -40,8 +42,9 @@ CREATE TABLE Renters (
   Complex_Number INT NOT NULL DEFAULT 000,
   Room_Number INT NOT NULL DEFAULT 0000,
   PRIMARY KEY (RSSN),
-  FOREIGN KEY (Complex_Number, Room_Number) REFERENCES Apartment(Complex_Number, Room_Number) 
-  ON DELETE SET DEFAULT
+  FOREIGN KEY (Complex_Number, Room_Number) 
+    REFERENCES Apartment(Complex_Number, Room_Number) 
+    ON DELETE SET DEFAULT
 );
 
 -- Lease Table
@@ -62,18 +65,29 @@ CREATE TABLE Contractors (
 CREATE TABLE Work_Order (
   Project_ID INT NOT NULL DEFAULT 00000,
   Type VARCHAR(255) DEFAULT NULL,
-  PRIMARY KEY (ProjectID)
+  Complex_Number INT NOT NULL DEFAULT 000,
+  -- TA said that work order should be a weak entity so composite key with project id and complex number
+  PRIMARY KEY (ProjectID, Complex_Number)
+  FOREIGN KEY (Complex_Number) 
+    REFERENCES Apartment_Complex(Complex_Number)
+    ON DELETE CASCADE
 );
 -- Commissions Table
 CREATE TABLE Commissions (
   Complex_Number INT NOT NULL DEFAULT 000,
   Company_Name VARCHAR(255) NOT NULL DEFAULT 'Not Provided',
-  Project_ID INT NOT NULL DEFAULT 00000,
-  Budget DECIMAL(10,2) DEFAULT 0, -- changed to decimal because its a price
+  Project_ID INT NOT NULL DEFAULT 00000, 
+  -- removed budget because its referenced in development projects
   Num_Hrs INT DEFAULT 0,
-  FOREIGN KEY (Complex_Number) REFERENCES Apartment_Complex(Complex_Number) ON DELETE SET DEFAULT,
-  FOREIGN KEY (Company_Name) REFERENCES Contractors(Company_Name) ON DELETE SET DEFAULT,
-  FOREIGN KEY (Project_ID) REFERENCES Work_Order(Project_ID) ON DELETE NO ACTION
+  FOREIGN KEY (Complex_Number) 
+    REFERENCES Apartment_Complex(Complex_Number) 
+    ON DELETE SET DEFAULT,
+  FOREIGN KEY (Company_Name) 
+    REFERENCES Contractors(Company_Name) 
+    ON DELETE SET DEFAULT,
+  FOREIGN KEY (Project_ID) 
+    REFERENCES Work_Order(Project_ID) 
+    ON DELETE NO ACTION
 );
 
 -- Development Projects Table 
@@ -81,16 +95,74 @@ CREATE TABLE Development_Projects (
   Project_ID INT NOT NULL DEFAULT 00000,
   Complex_Number INT NOT NULL DEFAULT 000,
   Budget DECIMAL(10,2) DEFAULT 0, -- changed to decimal because its a price
-  FOREIGN KEY (Project_ID) REFERENCES Work_Order(Project_ID) ON DELETE SET DEFAULT,
-  FOREIGN KEY (Complex_Number) REFERENCES Apartment_Complex(Complex_Number) ON DELETE SET DEFAULT,
+  FOREIGN KEY (Project_ID) 
+    REFERENCES Work_Order(Project_ID) 
+    ON DELETE SET DEFAULT,
+  FOREIGN KEY (Complex_Number) 
+    REFERENCES Apartment_Complex(Complex_Number) 
+    ON DELETE SET DEFAULT
 );
+
 -- Employees working at Apartment Complex Tavle 
+CREATE TABLE Employee_At_Complex (
+  ESSN SSN_TYPE NOT NULL DEFAULT '000-000-0000',
+  Complex_Number INT NOT NULL DEFAULT 000,
+  FOREIGN KEY (ESSN) 
+    REFERENCES Employee(ESSN) 
+    ON DELETE CASCADE,
+  FOREIGN KEY (Complex_Number) 
+    REFERENCES Apartment_Complex(Complex_Number) 
+    ON DELETE SET DEFAULT
+);
 
 -- Renters signing Leases Table 
+CREATE TABLE Renter_Sign_Lease (
+  RSSN SSN_TYPE NOT NULL '000-000-0000',
+  Contract_ID INT NOT NULL '000-0000',
+  FOREIGN KEY (RSSN)
+    REFERENCES Renters(RSSN)
+    ON DELETE RESTRICT,
+  FOREIGN KEY (Contract_ID)
+    REFERENCES Leases(Contract_ID)
+    ON DELETE RESTRICT
+);
 
 -- Employees fixing Residential Issues Table 
+CREATE TABLE Employee_Fix_Issue (
+  ESSN SSN_TYPE NOT NULL '000-000-0000',
+  Project_ID INT NOT NULL DEFAULT 00000,
+  FOREIGN KEY (ESSN) 
+    REFERENCES Employee(ESSN) 
+    ON DELETE SET DEFAULT,
+  FOREIGN KEY (Project_ID) 
+    REFERENCES Work_Order(Project_ID) 
+    ON DELETE RESTRICT
+};
 
 -- Renters Reporting Residential Issues Table 
+CREATE TABLE Renter_Report_Issue (
+  RSSN SSN_TYPE NOT NULL '000-000-0000',
+  Project_ID INT NOT NULL DEFAULT 00000,
+  FOREIGN KEY (RSSN)
+    REFERENCES Renters(RSSN)
+    ON DELETE RESTRICT,
+  FOREIGN KEY (Project_ID) 
+    REFERENCES Work_Order(Project_ID) 
+    ON DELETE RESTRICT
+);
 
 -- Residential Property Issues Table 
-
+CREATE TABLE Property_Issues (
+  Project_ID INT NOT NULL DEFAULT 00000, -- changed from issue_id to project_id b/c its useless otherwise
+  RSSN SSN_TYPE NOT NULL '000-000-0000',
+  Complex_Number INT NOT NULL DEFAULT 000,
+  FOREIGN KEY (Project_ID) 
+    REFERENCES Work_Order(Project_ID) 
+    ON DELETE RESTRICT,
+  FOREIGN KEY (RSSN)
+    REFERENCES Renters(RSSN)
+    ON DELETE RESTRICT,
+  FOREIGN KEY (Complex_Number) 
+    REFERENCES Apartment_Complex(Complex_Number) 
+    ON DELETE SET DEFAULT
+);
