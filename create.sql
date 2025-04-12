4e6e62c2c718ea99406a770e4067babbdaef04a2
sanahsp <137027422+sanahsp@users.noreply.github.com>
2025-04-12 13:41:07 -0500

    Update create.sql

--- create.sql
+++ create.sql
@@ -16,7 +16,7 @@ CREATE TABLE Apartment_Complex (
   Complex_Number INT NOT NULL DEFAULT 0,
   Street VARCHAR(155) DEFAULT NULL,
   City VARCHAR(100) DEFAULT NULL,
-  Addr_State VARCHAR(50) DEFAULT NULL, -- i changed this to Addr_State because state is a keyword
+  Addr_State VARCHAR(50) DEFAULT NULL, 
   Manager SSN_TYPE NOT NULL DEFAULT '000-000-0000',
   PRIMARY KEY (Complex_Number),
   FOREIGN KEY (Manager)
@@ -50,7 +50,7 @@ CREATE TABLE Renters (
 -- lease type table
 CREATE TABLE Lease_Type (
   Type INT DEFAULT NULL,
-  Payment_Amount DECIMAL(10,2) DEFAULT 0, -- changed to decimal because its a price
+  Payment_Amount DECIMAL(10,2) DEFAULT 0, 
   PRIMARY KEY (Type)
 );
 
@@ -75,7 +75,6 @@ CREATE TABLE Work_Order (
   Project_ID INT NOT NULL DEFAULT 0,
   Type VARCHAR(255) DEFAULT NULL,
   Complex_Number INT NOT NULL DEFAULT 0,
-  -- TA said that work order should be a weak entity so composite key with project id and complex number
   PRIMARY KEY (Project_ID, Complex_Number),
   FOREIGN KEY (Complex_Number) 
     REFERENCES Apartment_Complex(Complex_Number)
@@ -86,7 +85,6 @@ CREATE TABLE Commissions (
   Complex_Number INT NOT NULL DEFAULT 0,
   Company_Name VARCHAR(255) NOT NULL DEFAULT 'Not Provided',
   Project_ID INT NOT NULL DEFAULT 0,
-  -- removed budget because its referenced in development projects
   Num_Hrs INT DEFAULT 0,
   PRIMARY KEY (Complex_Number, Company_Name, Project_ID)
   FOREIGN KEY (Complex_Number) 
@@ -103,14 +101,14 @@ CREATE TABLE Commissions (
 -- Development Projects Table 
 CREATE TABLE Development_Projects (
   Project_ID INT NOT NULL DEFAULT 0,
-  Budget DECIMAL(10,2) DEFAULT 0, -- changed to decimal because its a price
+  Budget DECIMAL(10,2) DEFAULT 0, 
   Complex_Number INT NOT NULL DEFAULT 0,
   FOREIGN KEY (Project_ID, Complex_Number)
     REFERENCES Work_Order(Project_ID, Complex_Number)
     ON DELETE RESTRICT
 );
 
--- Employees working at Apartment Complex Tavle 
+-- Employees working at Apartment Complex Table
 CREATE TABLE Employee_At_Complex (
   ESSN SSN_TYPE NOT NULL DEFAULT '000-000-0000',
   Complex_Number INT NOT NULL DEFAULT 0,
@@ -166,7 +164,7 @@ CREATE TABLE Renter_Report_Issue (
 
 -- Residential Property Issues Table 
 CREATE TABLE Property_Issues (
-  Project_ID INT NOT NULL DEFAULT 0, -- changed from issue_id to project_id b/c its useless otherwise
+  Project_ID INT NOT NULL DEFAULT 0, 
   Description VARCHAR(255) DEFAULT 'Needs Description',
   Complex_Number INT NOT NULL DEFAULT 0,
   FOREIGN KEY (Project_ID, Complex_Number)
