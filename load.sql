eedd4608f43cf523ca9708933c7c89307d031397
sanahsp <137027422+sanahsp@users.noreply.github.com>
2025-04-13 01:55:41 -0500

    Update load.sql

--- load.sql
+++ load.sql
@@ -1,1 +1,7 @@
-
+LOAD DATA LOCAL INFILE 'sql-database-project/Employee.csv'
+INTO TABLE Employee
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(ESSN, Salary, Name);
